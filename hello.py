3b143bacfa942250e52d53d690ffb36b450d1da9
jack-hug <46361381@qq.com>
2019-08-01 17:20:42 +0800

    集成Python shell

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,session,redirect,url_for,flash
-from flask_script import Manager
+from flask_script import Manager,Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -42,6 +42,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+def make_shell_context():
+    return dict(app = app ,db = db ,User = User,Role = Role)
+manager.add_command('shell',Shell(make_context=make_shell_context))
+
+
 @app.route('/',methods = ['GET','POST'])
 def index():
     form = NameForm()
@@ -60,7 +65,7 @@ def index():
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',form = form,name = session.get('name'),known = session.get['known',False],current_time = datetime.utcnow())
+    return render_template('index.html',form = form,name = session.get('name'),known = session.get('known',False),current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
