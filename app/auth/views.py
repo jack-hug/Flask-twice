fe0a5144a8e0897a52436186c467fca486de7ef7
jack-hug <46361381@qq.com>
2019-08-05 17:28:08 +0800

    注册新用户

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template,url_for,request,flash,redirect
 from flask_login import login_user,login_required,logout_user
 from ..models import User
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
 @auth.route('/login',methods = ['GET','POST'])
 def login():
@@ -14,6 +15,17 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('无效的用户名或密码')
     return render_template('auth/login.html',form = form)
+
+@auth.route('/register', methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('注册成功，现在你可以用此邮箱登录')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form = form)
 
 @auth.route('/logout')
 @login_required
